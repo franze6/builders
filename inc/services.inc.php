c0b8f67daee78dbf224d81702a2fa0da60533da5
Kirill <soldatmt2@mail.ru>
2017-10-16 18:30:00 +0300

    Сделал раздел услуг

---
+++ inc/services.inc.php
@@ -0,0 +1,49 @@
+<div class="services_section_block">
+    <div class="three_block">
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#tile_work" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/tile_work.jpg" alt="Плиточные работы" title="Плиточные работы" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#tile_work" itemprop="name">Плиточные работы</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#plumbing_work" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/plumbing_work.jpg" alt="Другие ремонтные работы" title="Другие ремонтные работы" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#plumbing_work" itemprop="name">Сантехнические работы</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#footer_work" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/footer_work.jpg" alt="Натяжные потолки" title="Натяжные потолки" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#footer_work" itemprop="name">Потолочные работы</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+    </div>
+</div>
