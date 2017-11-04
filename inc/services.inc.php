c71e8d778f5c49084fd9e60eba9b4fe8ad0948c9
fanze6 <soldatmt2@mail.ru>
2017-11-04 14:05:41 +0300

    123

--- inc/services.inc.php
+++ inc/services.inc.php
@@ -1,6 +1,21 @@
 <div class="services_section_block">
     <div class="three_block">
         <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#plaster_work" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/plaster_work.jpg" alt="Штукатурные работы" title="Штукатурные работы" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#plaster_work" itemprop="name">Штукатурные работы</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
             <a href="/price/#tile_work" class="animation_img">
                 <span class="overlay"></span>
                 <img src="/img/services/tile_work.jpg" alt="Плиточные работы" title="Плиточные работы" itemprop="image">
@@ -30,10 +45,12 @@
                 </div>
             </div>
         </article>
+    </div>
+    <div class="three_block">
         <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
             <a href="/price/#footer_work" class="animation_img">
                 <span class="overlay"></span>
-                <img src="/img/services/footer_work.jpg" alt="Натяжные потолки" title="Натяжные потолки" itemprop="image">
+                <img src="/img/services/footer_work.jpg" alt="Сварочные работы" title="Сварочные работы" itemprop="image">
                 <span class="link"><i class="fa fa-link"></i></span>
             </a>
             <div class="description">
@@ -45,8 +62,6 @@
                 </div>
             </div>
         </article>
-    </div>
-    <div class="three_block">
         <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
             <a href="/price/#welding_work" class="animation_img">
                 <span class="overlay"></span>
@@ -56,6 +71,38 @@
             <div class="description">
                 <h2>
                     <a rel="bookmark" href="/price/#welding_work" itemprop="name">Сварочные работы</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#unfire_work" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/unfire_work.jpg" alt="Противопожарная безопасность" title="Противопожарная безопасность" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#unfire_work" itemprop="name">Противопожарная безопасность</a>
+                </h2>
+                <div itemprop="description">
+                    Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="single_block">
+        <article class="services_section_item" itemscope="" itemtype="http://schema.org/Article">
+            <a href="/price/#recycling" class="animation_img">
+                <span class="overlay"></span>
+                <img src="/img/services/recycling.jpg" alt="Работы по утилизации отходов" title="Работы по утилизации отходов" itemprop="image">
+                <span class="link"><i class="fa fa-link"></i></span>
+            </a>
+            <div class="description">
+                <h2>
+                    <a rel="bookmark" href="/price/#recycling" itemprop="name">Работы по утилизации отходов</a>
                 </h2>
                 <div itemprop="description">
                     Просто отказываться от&nbsp;такого красивого, практичного и&nbsp;экологичного материала, как дерево, нерационально и&nbsp;неразумно.
